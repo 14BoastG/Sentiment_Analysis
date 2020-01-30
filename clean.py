a83b0f8d5a9b3f0fa908b170511dc5af9d63b400
George Boast <14boastg@brentwood.essex.sch.uk>
2020-01-30 11:58:21 +0000

    added main.py with function call

---
+++ clean.py
@@ -0,0 +1,39 @@
+def Cleaner(text):
+
+  # import/donwload relevant packages
+  import string
+  import nltk
+  nltk.download('punkt')
+  nltk.download('stopwords')
+
+  # split into words
+  from nltk.tokenize import word_tokenize
+  tokens = word_tokenize(text)
+  print("Tokens = " , tokens)
+
+  # convert to lower case
+  tokens = [w.lower() for w in tokens]
+
+  # remove punctuation from each word
+  table = str.maketrans('', '', string.punctuation)
+  stripped = [w.translate(table) for w in tokens]
+  print("Stripped = " , stripped)
+
+  # remove remaining tokens that are not alphabetic
+  words = [word for word in stripped if word.isalpha()]
+
+  # filter out stop words
+  from nltk.corpus import stopwords
+  stop_words = set(stopwords.words('english'))
+  words = [w for w in words if not w in stop_words]
+  print("Words = " , words)
+
+  # stemming of words
+  from porter2stemmer import Porter2Stemmer
+  stemmer = Porter2Stemmer()
+  stemmed = [stemmer.stem(word) for word in words]
+
+  # final output
+  global clean_text
+  clean_text = stemmed
+  return( "Clean Text = ", clean_text)
---
+++ main.py
@@ -0,0 +1,6 @@
+# import/donwload relevant packages
+from clean import Cleaner
+
+# passes text to Cleaner
+text = input("Raw text: ")
+Cleaner(text)
